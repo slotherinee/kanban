d2a490d907a872d77c3e1fff21d7352f2add2ac2
Pavel Tarasov <paveltarasov121@gmail.com>
2025-09-28 02:08:27 +0300

    prisma setup

---
+++ prisma/migrations/20250927230128_users_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "public"."users" (
+    "id" UUID NOT NULL,
+    "username" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
